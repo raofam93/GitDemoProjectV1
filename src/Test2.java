8d2e3225fdda4c1f6f5e06112cf920dbbb0319d5
ravi <ravirao605@gmail.com>
2022-05-08 21:00:48 -0700

    Commiting test 2 to branchx

---
+++ src/Test2.java
@@ -0,0 +1,20 @@
+
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry.equals("yes")) {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("do work");
+			
+		}
+
+	}
+
+}
