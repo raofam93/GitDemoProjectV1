611559c68177bfe502f4dc27a334b45d279dd17c
tribalpages <tribalpages@tribalpages-PC.lan>
2022-05-08 18:39:06 -0700

    First Commit ....

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry.equals("yes")) {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("do work");
+			
+		}
+
+	}
+
+}
